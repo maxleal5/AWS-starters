cc466a3e26587556ba583f08b77f8e3af46308f2
Max Leal <maxbleal@gmail.com>
2024-06-02 02:08:17 +0000

    ruby sdk s3

---
+++ s3/sdk/ruby/s3.rb
@@ -0,0 +1,39 @@
+require 'aws-sdk-s3'
+require 'pry'
+require 'securerandom'
+
+bucket_name = ENV['BUCKET_NAME']
+region = 'us-east-2'
+
+client = Aws::S3::Client.new()
+
+resp = client.create_bucket({
+  bucket: bucket_name, 
+  create_bucket_configuration: {
+    location_constraint: region, 
+  } 
+})
+#binding.pry
+
+number_of_files = 1 + rand(6)
+puts "number_of_files: #{number_of_files}"
+
+
+number_of_files.times.each do |i|
+    puts "i: #{i}"
+    filename = "file_#{i}.txt"
+    output_path = "/tmp/#{filename}"
+
+    File.open(output_path, "w") do |f|
+        f.write SecureRandom.uuid
+    end
+
+    File.open(output_path, 'rb') do |f|
+        client.put_object(
+            bucket: bucket_name,
+            key: filename,
+            body: f
+        )
+    end
+
+end
